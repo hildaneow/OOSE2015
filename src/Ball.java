14238d6eda6f6d87814b6b31aa5dfc17e029d53c
rasmusmm <rasmus.mm@gmail.com>
2015-03-30 14:50:33 +0200

    Ball and bricks classes.

---
+++ src/Ball.java
@@ -0,0 +1,14 @@
+// Import Timer and other useful stuff:
+import java.util.*;
+// Import the basic graphics classes.
+import java.awt.*;
+import javax.swing.*;
+import java.lang.Math;
+
+
+public class Ball {
+	int posX;
+	int posY;
+	int Width;
+	int Height;
+}
---
+++ src/Bricks.java
@@ -0,0 +1,22 @@
+// Import Timer and other useful stuff:
+import java.util.*;
+// Import the basic graphics classes.
+import java.awt.*;
+import javax.swing.*;
+import java.lang.Math;
+
+public class Bricks {
+	int posX;
+	int posY;
+	int Width;
+	int Height;
+
+public void OnCollision(){
+	if (this.Bricks.posX+this.Bricks.Width/2 <Ball.posX && this.Bricks.posY+this.Bricks.Height/2 <Ball.posY)
+	
+}	
+
+
+
+}
+
